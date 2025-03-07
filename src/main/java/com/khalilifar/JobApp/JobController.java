dba7ee12ff653c070628f2976209d6387bfefe00
Roxy Khalilifar <roxykhal@hotmail.com>
2025-03-07 14:16:47 +0000

    add get and post mapping requests

--- src/main/java/com/khalilifar/JobApp/JobController.java
+++ src/main/java/com/khalilifar/JobApp/JobController.java
@@ -1,22 +1,24 @@
 package com.khalilifar.JobApp;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
 
-    @RequestMapping({"/", "home"})
+    @GetMapping({"/", "home"})
     public String home() {
         return "home";
     }
 
-    @RequestMapping("addjob")
+    @GetMapping("addjob")
     public String addJob() {
         return "addjob";
 
     }
 
-    @RequestMapping("handleForm")
+    @PostMapping("handleForm")
         public String handleForm() {
             return "success";
 
--- src/main/java/com/khalilifar/JobApp/model/JobPost.java
+++ src/main/java/com/khalilifar/JobApp/model/JobPost.java
@@ -1,7 +1,16 @@
 package com.khalilifar.JobApp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
 public class JobPost {
     private int postId;
     private String postProfile;
