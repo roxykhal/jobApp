331e115d346220cd467aebfb7d990eac5e9071bc
Roxy Khalilifar <roxykhal@hotmail.com>
2025-03-07 14:11:29 +0000

    new job post class and the variables sent to form

---
+++ src/main/java/com/khalilifar/JobApp/model/JobPost.java
@@ -0,0 +1,11 @@
+package com.khalilifar.JobApp.model;
+
+import java.util.List;
+
+public class JobPost {
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private int reqExperience;
+    private List<String> postTechStack;
+}
